e3b7b820cfd6f00eccadbfb75c964120d1314a66
Olsen Kraja <krajaolsen@gmail.com>
2024-12-23 14:53:11 +0100

    Debugging github pages bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://olsenkraja.github.io',
   integrations: [react(), markdoc(), vue(), keystatic(), mdx()],
-  output: 'static'
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
